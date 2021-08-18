feb00bd2d6e68cd1e04cf550999c49c4c42393af
Jordan <jordansoh14@gmail.com>
2021-08-18 12:08:28 -0500

    got it to work

--- Develop/script.js
+++ Develop/script.js
@@ -33,11 +33,10 @@ function generatePassword() {
   let isSpec = window.confirm("Would you like to include Special characters?");
   console.log(isSpec)
 
-  // for conditionals need to make sure what we are checking is relevant to the data type we're comparing to
-  // our goal is IF a user said yes to a confirm prompt that the contents of that array are added/appended/pushed to our empty userChar array
+
 
   if (isNum) {
-    // if this evaluates to true then we need to add the contents of the num array userNum to the userChar array
+
     userChar = userChar.concat(userNum)
     console.log("new array", userChar)
 
@@ -60,27 +59,23 @@ function generatePassword() {
     console.log("new array ", userChar)
 
   }
-
+  let passwordPrint = [];
 
   for (let i = 0; i < chosenPassword; i++) {
-    let randomNumber = Math.floor(Math.random() * userChar.length);
-    let newPass = [];
-    newPass.push(userChar[randomNumber]);
-    console.log(newPass);
-    
+    console.log(Math.floor(Math.random() * userChar.length));
+    let randomNumber = (Math.floor(Math.random() * userChar.length));
+    let generatedIndex = userChar[randomNumber]
+    console.log(generatedIndex)
+    passwordPrint.push(generatedIndex)
   }
+  console.log(passwordPrint)
+  return passwordPrint.join("");
   
 }
 
 
 
 
-  // after we've added all of the new characters that the user wanted into the the empty array, we need to start looping and grabbing characters from that array based on a random index
-  // each character will be grabbed one at a time and we need to store these as a string as the final password
-  // you can use concat here as well so we concat the new character on the variable each time
-
-  //  dont forget we are indexing through the new array we just made and we need to store the resultant concatenation in a new variable
-  // for (let i = 0; i < passwordLength; i++) {
 
 
 
